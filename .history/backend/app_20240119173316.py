bb02f495e316b1dda9cc922d18e5877f5adc7507
Yousuf Mesalm <31203259+Youssefmesalm@users.noreply.github.com>
2024-01-22 21:26:47 +0200

    Fix Cros

--- .history/backend/app_20240119173316.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from admin.api.users_management_api import router as users_management_router
-
-app = FastAPI()
-
-app.include_router(users_management_router, prefix="/users", tags=["Users"])
-
-
-
