6bf8aae3ac844a0af0f7dd42cca467746ea0a318
Yousuf Mesalm <31203259+Youssefmesalm@users.noreply.github.com>
2024-01-22 20:39:13 +0200

    frontend

--- .history/backend/app_20240119181917.py
+++ .history/backend/app_20240119181917.py
@@ -3,12 +3,7 @@ from admin.api.users_management_api import router as users_management_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+
 app.include_router(users_management_router, prefix="/users", tags=["Users"])
 
 
