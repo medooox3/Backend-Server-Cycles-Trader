423757e4926f2ce8b6e8ceb01bda0965b65dfa96
Mohamed <medooox33@gmail.com>
2024-01-23 01:10:17 +0200

    .

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from users_management import router as users_management_router
 from admin import router as admin_router
 from security.web import auth_api
-
 
 
 app = FastAPI()
@@ -14,17 +13,21 @@ app.include_router(
         Depends(auth_api.admin_oauth2_scheme),
     ],
 )
-app.include_router(admin_router, prefix="/admin", dependencies=[
-    Depends(auth_api.admin_oauth2_scheme),
-])
+app.include_router(
+    admin_router,
+    prefix="/admin",
+    dependencies=[
+        Depends(auth_api.admin_oauth2_scheme),
+    ],
+)
 app.include_router(auth_api.router, prefix="/token")
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
