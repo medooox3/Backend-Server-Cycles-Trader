8160f499b6bed75db170aaa8080cb4c03570c7fc
Mohamed <medooox33@gmail.com>
2024-01-25 00:14:27 +0200

    update me path

--- user/me/web/me.py
+++ user/me/web/me.py
@@ -5,7 +5,7 @@ from security.web import auth_api
 router = APIRouter()
 
 
-@router.get("", response_model=UserRead, description='Return this logged in user info')
+@router.get("/", response_model=UserRead, description='Return this logged in user info')
 async def me(user: UserRead = Depends(auth_api.get_user)):
     return user
 
