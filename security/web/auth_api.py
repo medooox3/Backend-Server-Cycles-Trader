d4fe374546d4c650c19dab768ca4cac25805dc78
Mohamed <medooox33@gmail.com>
2024-01-24 20:33:27 +0200

    create inital user auth

--- security/web/auth_api.py
+++ security/web/auth_api.py
@@ -11,8 +11,13 @@ from admin.data import admin_repo
 from ..utils import jwt_utils, password_utils
 
 
+from users_management.data import user_repo
+from users_management.data.user import User
+
+
 router = APIRouter(tags=["Authentication"])
 admin_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token/admin")
+user_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token/user")
 
 
 @router.post("/admin")
@@ -47,3 +52,46 @@ def get_admin(session: DBSession, token: Annotated[str, Depends(admin_oauth2_sch
     admin = admin_repo.get_admin_using_email(session, token_data.sub)
     # ? Do i need to return the admin ?
     return admin
+
+# * --------- User Auth ---------------- *
+@router.post("/user")
+async def user_login_for_token(
+    session: DBSession,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    UnAuthorizedException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+    )
+    
+    user = user_repo.get_user_by_name(session, form_data.username)
+    if not user:
+        raise UnAuthorizedException
+    if not password_utils.verify_password(form_data.password, user.password_hash):
+        raise UnAuthorizedException
+
+    #  Todo: Later i can modify the token data to accept the session number to be able to count and 
+    # revoke a session from the backend
+    access_token = jwt_utils.create_access_token(
+        token_data=TokenData(sub=user.name, admin=False)
+    )
+    token = Token(access_token=access_token)
+    return token
+    # return {"access_token": payload, "token_type": "bearer"}
+
+def get_user(session: DBSession, token: Annotated[str, Depends(user_oauth2_scheme)]):
+    """a dependency to get the user from the DB, injecting this function
+    into the router of the routes that need user authorization.   
+    
+    Using this returned user from the dependency all the coming operations will use his id to perform api operations.
+    For example all the functions to create  a cycle will require a user id in the argument
+    list and using this id will insert the data in the database and link the user to them.
+    """
+    
+    token_data = jwt_utils.verify_token_access(token)
+    user = user_repo.get_user_by_name(session, token_data.sub)
+    # ^ Using this returned user all the coming operations will use his id to perform api operations ^
+    # For example all the functions to create a license or a cycle will require a user id in the argument
+    # list and using this id will insert the data in the database and link the user to them.
+    return user
+
